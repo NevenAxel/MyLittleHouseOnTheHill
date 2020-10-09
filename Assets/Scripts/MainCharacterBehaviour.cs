87f0339ddbc10bb613fd00175dfce41e643304a8
LAPTOP-A38KUSPF\pro <catambroise@hotmail.fr>
2020-10-10 01:44:40 +0200

    Player move

---
+++ Assets/Scripts/MainCharacterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.other.CompareTag("Tree"))
+        {
+
+        }
+    }
+}
