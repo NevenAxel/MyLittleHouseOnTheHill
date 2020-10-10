f7090c9c0f1b2bc506a0887e7471434f97f4ecee
Neven Axel <neven.axel@gmail.com>
2020-10-10 14:12:54 +0200

    Achievement Done, Quest WIP

--- Assets/Scripts/AchievementHandler.cs
+++ Assets/Scripts/AchievementHandler.cs
@@ -6,35 +6,26 @@ using UnityEngine;
 public class AchievementHandler : MonoBehaviour
 {
     [SerializeField] AchievementUI achievementUIprefab;
+    AchievementUI currentAchievementPrefab;
     [SerializeField] float achievementStayDuration = 1f;
+    [SerializeField] float progressBarDuration = 0.25f;
+    float countDownToHide = 1f;
 
     MainCharacterBehaviour player;
 
     [SerializeField] TextAsset woodcutAchievementData;
     int woodCut;
-    [SerializeField] List<Achievement> woodCutAchievements = new List<Achievement>();
+    [SerializeField] List<Completition> woodCutAchievements = new List<Completition>();
 
-    [System.Serializable]
-    class Achievement
-    {
-        
-        public string _name;
-        public string _description;
-        public Sprite _sprite;
-        public int Milestone;
-        public Achievement(string name, string description, int milestone)
-        {
-            _description = description;
-            _name = name;
-            Milestone = milestone;
-        }
-    }
+
+
+    
 
     void Start()
     {
         player = FindObjectOfType<MainCharacterBehaviour>();
         player.onWoodChopped += OnWoodChopped;
-        
+        countDownToHide = achievementStayDuration;
     }
 
     [ContextMenu("TranslateWoodCutAchievement")]
@@ -44,7 +35,7 @@ public class AchievementHandler : MonoBehaviour
     }
 
 
-    private void TranslateAchievmentData(TextAsset data, List<Achievement> achievementList)
+    private void TranslateAchievmentData(TextAsset data, List<Completition> achievementList)
     {
         string[] lines = data.text.Split(new char[] { '\n' });
         // skip first line
@@ -52,7 +43,7 @@ public class AchievementHandler : MonoBehaviour
         {
             string[] cases = lines[i].Split(new char[] { ',' });
 
-            Achievement achievement = new Achievement(cases[0], cases[1], Int32.Parse(cases[2]));
+            Completition achievement = new Completition(cases[0], cases[1], Int32.Parse(cases[2]));
             achievementList.Add(achievement);
         }
     }
@@ -63,7 +54,7 @@ public class AchievementHandler : MonoBehaviour
         achievementCheck(woodCutAchievements, woodCut);
     }
 
-    private void achievementCheck(Achievement achievement, int currentAdvancement)
+    private void achievementCheck(Completition achievement, int currentAdvancement)
     {
         if (achievement.Milestone == currentAdvancement)
         {
@@ -71,46 +62,105 @@ public class AchievementHandler : MonoBehaviour
         }
         else
         {
-            achievementUpdated(achievement);
+            achievementUpdated(achievement, 0, currentAdvancement);
         }
     }
 
-    private void achievementCheck(List<Achievement> achievements, int currentAdvancement)
+    private void achievementCheck(List<Completition> achievements, int currentAdvancement)
     {
-        foreach(Achievement achievement in achievements)
+        for(int i = 0; i < achievements.Count; i++)
         {
-            if(achievement.Milestone == currentAdvancement)
+            if (achievements[i].Milestone == currentAdvancement)
             {
-                achievementUnlocked(achievement);
+                achievementUnlocked(achievements[i]);
                 return;
             }
-            if (achievement.Milestone > currentAdvancement)
+            else if(i == 0 && achievements[0].Milestone > currentAdvancement)
             {
-                achievementUpdated(achievement);
+                    achievementUpdated(achievements[i], 0, currentAdvancement);
+                    return;
+            }
+            else if(i == achievements.Count && achievements[i - 1].Milestone < currentAdvancement)
+            {
+                    achievementUpdated(achievements[i], achievements[i-1].Milestone, currentAdvancement);
+                    return;
+            }
+            else if (achievements[i].Milestone < currentAdvancement && achievements[i + 1].Milestone > currentAdvancement)
+            {
+
+                achievementUpdated(achievements[i+1], achievements[i].Milestone, currentAdvancement);
                 return;
             }
         }
     }
 
-    private void achievementUpdated(Achievement achievement)
+    private void achievementUpdated(Completition achievement, int previousMilestone, int advancement)
     {
-        AchievementUI achievementPrefab = Instantiate(achievementUIprefab, gameObject.transform);
-        achievementPrefab.achievementDescription.text = achievement._description;
-        achievementPrefab.achievmentTitle.text = achievement._name;
-        achievementPrefab.achievementImage.sprite = achievement._sprite;
-        Animation anim = achievementPrefab.GetComponent<Animation>();
-        anim.Play();
-        Destroy(achievementPrefab.gameObject, achievementStayDuration);
+        if(currentAchievementPrefab == null)
+        {
+            StartCoroutine(Fade());
+            currentAchievementPrefab = Instantiate(achievementUIprefab, gameObject.transform);           
+            Animation anim = currentAchievementPrefab.GetComponent<Animation>();
+            anim.Play("AchievementUpdate");
+        }
+        else
+        {
+            countDownToHide = achievementStayDuration;
+        }
+            
+        currentAchievementPrefab.achievementDescription.text = achievement._description;
+        currentAchievementPrefab.achievmentTitle.text = achievement._name;
+        currentAchievementPrefab.achievementImage.sprite = achievement._sprite;
+
+        float normalizedStartingAdvancement = (float)(advancement - 1 - previousMilestone) / (float)(achievement.Milestone - previousMilestone);
+        float normalizedAdvancement = (float)(advancement - previousMilestone) / (float)(achievement.Milestone - previousMilestone);
+        StartCoroutine(LerpSize(currentAchievementPrefab.progressBar.transform, normalizedStartingAdvancement, normalizedAdvancement, progressBarDuration, progressBarDuration));
+        StartCoroutine(LerpSize(currentAchievementPrefab.progressBarTemp.transform, normalizedStartingAdvancement, normalizedAdvancement, progressBarDuration));
     }
 
-    private void achievementUnlocked(Achievement achievement)
+    private void achievementUnlocked(Completition achievement)
     {
-        AchievementUI achievementPrefab = Instantiate(achievementUIprefab, gameObject.transform);
-        achievementPrefab.achievementDescription.text = achievement._description;
-        achievementPrefab.achievmentTitle.text = achievement._name;
-        achievementPrefab.achievementImage.sprite = achievement._sprite;
-        Animation anim = achievementPrefab.GetComponent<Animation>();
-        anim.Play();
-        Destroy(achievementPrefab.gameObject, achievementStayDuration);
+        if (currentAchievementPrefab == null)
+        {
+            StartCoroutine(Fade());
+            currentAchievementPrefab = Instantiate(achievementUIprefab, gameObject.transform);
+            Animation anim = currentAchievementPrefab.GetComponent<Animation>();           
+            anim.Play("AchievementUnlockedNew");
+        }
+        else
+        {
+            countDownToHide = achievementStayDuration;
+            Animation anim = currentAchievementPrefab.GetComponent<Animation>();
+            anim.Play("AchievementUnlocked");
+        }
+            
+        currentAchievementPrefab.achievementDescription.text = achievement._description;
+        currentAchievementPrefab.achievmentTitle.text = achievement._name;
+        currentAchievementPrefab.achievementImage.sprite = achievement._sprite;
+        currentAchievementPrefab.progressBar.transform.localScale = new Vector3(1, 1, 1);
+    }
+    IEnumerator LerpSize(Transform transform, float origin, float endPoint, float duration, float delay = 0f)
+    {
+        float timer = 0;
+        yield return new WaitForSeconds(delay);
+        while(timer < duration)
+        {
+            timer += Time.deltaTime;
+            float scale = Mathf.Lerp(origin, endPoint, timer / duration);
+            transform.localScale = new Vector3(scale, 1, 1);           
+            yield return null;
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        for (int i = 0; i < countDownToHide; i++)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        Animation anim = currentAchievementPrefab.GetComponent<Animation>();
+        anim.Play("AchievementHide");
+        currentAchievementPrefab = null;
+        Destroy(currentAchievementPrefab, 1f);
     }
 }
