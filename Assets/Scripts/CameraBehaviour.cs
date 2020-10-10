32ae85171f88d92e93db1c474b3fbe8e31802a19
LAPTOP-A38KUSPF\pro <catambroise@hotmail.fr>
2020-10-10 04:44:01 +0200

    camera follow the player after x tree cut

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    bool isParentedToPlayer;
+    [SerializeField]
+    GameObject positionToFollow;
+    [SerializeField]
+    float lerp;
+    [SerializeField]
+    int firstMissionWood;
+    private void Awake()
+    {
+        FindObjectOfType<MainCharacterBehaviour>().onWoodChopped += OnWoodChopped;
+    }
+
+    public void OnWoodChopped(object sender, MainCharacterBehaviour.OnWoodChoppedEventArgs e)
+    {
+        if (e.wood > firstMissionWood)
+            isParentedToPlayer = true;
+    }
+
+    private void LateUpdate()
+    {
+        if(isParentedToPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, positionToFollow.transform.position, lerp);
+        }
+    }
+}
