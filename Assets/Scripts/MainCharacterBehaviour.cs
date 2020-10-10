06f5d5e54d7baf5f10995c3997d1b0fbcd01ab90
LAPTOP-A38KUSPF\pro <catambroise@hotmail.fr>
2020-10-10 02:30:50 +0200

    Chop wood

--- Assets/Scripts/MainCharacterBehaviour.cs
+++ Assets/Scripts/MainCharacterBehaviour.cs
@@ -6,21 +6,50 @@ public class MainCharacterBehaviour : MonoBehaviour
 {
     [SerializeField]
     float speed;
+    [SerializeField]
+    int baseChopForce;
+    [SerializeField]
+    int chopRandom;
+    [SerializeField]
+    int woodAdd;
+    [SerializeField]
+    int woodAddRandom;
+    [SerializeField]
+    float timeChop;
+    float timeLastChop;
+    int currentWood;
     void Start()
     {
-        
+        timeLastChop = Time.time - timeChop;
     }
-    void Update()
+    void FixedUpdate()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.position += move * speed * Time.deltaTime;
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnTrigerStay(Collider collider)
     {
-        if(collision.other.CompareTag("Tree"))
+        if (!Input.GetKey(KeyCode.Space))
+            return;
+        Wood wood = collider.GetComponent<Wood>();
+        if (wood != null)
         {
-
+            if(timeLastChop < Time.time - timeChop)
+            {
+                Chop(wood);
+            }
         }
+    }
+    void Chop(Wood wood)
+    {
+        timeLastChop = Time.time;
+        int forceChop = baseChopForce + Random.Range(0, chopRandom + 1);
+        if (wood.GetChopped(forceChop))
+        {
+            currentWood += woodAdd + Random.Range(0, woodAddRandom+1);
+            Debug.Log(currentWood);
+        }
+
     }
 }
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Wood : MonoBehaviour
+{
+    [SerializeField]
+    int baseLife;
+
+    int currentLife;
+    [SerializeField]
+    GameObject parentLife;
+    [SerializeField]
+    Image woundBar;
+    private void Awake()
+    {
+        currentLife = baseLife;
+    }
+    public bool GetChopped(int chopForce)
+    {
+        currentLife -= chopForce;
+        SetUiLife();
+        if(currentLife <= 0)
+        {
+            Destroy(this.gameObject);
+            return true;
+        }
+        return false;
+    }
+    public void SetUiLife()
+    {
+        parentLife.SetActive(true);
+        woundBar.rectTransform.offsetMin = new Vector2((float)currentLife/(float)baseLife, woundBar.rectTransform.offsetMin.y);
+    }
+}
