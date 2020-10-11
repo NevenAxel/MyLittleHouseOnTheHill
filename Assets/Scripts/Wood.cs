f1bf323105d351303db3d6fed90d84ba5df299fc
Neven Axel <neven.axel@gmail.com>
2020-10-11 03:46:02 +0200

    Finished tronk behavior + wip game design

--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -6,17 +6,26 @@ using UnityEngine.UI;
 public class Wood : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    [SerializeField] GameObject fx;
+    [SerializeField] GameObject fxDestroy;
+    [SerializeField] GameObject fxHit;
+
+    [SerializeField] MeshRenderer originalMeshRend;
+    [SerializeField] MeshFilter originalMeshFilter;
+    [SerializeField] Material tronkMaterial;
+    [SerializeField] Mesh tronkMesh;
+
     [SerializeField]
     int baseLife;
     [SerializeField]
     UnityEvent onChopped;
+    Collider col;
 
     int currentLife;
     [SerializeField]
     Bar lifeBar;
     private void Awake()
     {
+        col = GetComponent<Collider>();
         currentLife = baseLife;
     }
     public bool GetChopped(int chopForce)
@@ -24,17 +33,21 @@ public class Wood : MonoBehaviour
         animator.SetTrigger("onHit");
         currentLife -= chopForce;
         lifeBar.SetUi((float)currentLife / (float)baseLife);
-        if(currentLife <= 0)
+        GameObject FX = Instantiate(fxHit, gameObject.transform.position, Quaternion.identity);
+        Destroy(FX, 2f);
+        if (currentLife <= 0)
         {
+            originalMeshRend.material = tronkMaterial;
+            originalMeshFilter.mesh = tronkMesh;
             // fx
-
-            GameObject newFX = Instantiate(fx, gameObject.transform.position, Quaternion.identity);
+            GameObject newFX = Instantiate(fxDestroy, gameObject.transform.position, Quaternion.identity);
             Destroy(newFX, 2f);
-            Destroy(animator.gameObject, 2);
-            Destroy(gameObject);
-
+            
+            
             onChopped.Invoke();
-            Destroy(this.gameObject);
+            lifeBar.gameObject.SetActive(false);
+            col.enabled = false;
+            this.enabled = false;
             return true;
         }
         return false;
