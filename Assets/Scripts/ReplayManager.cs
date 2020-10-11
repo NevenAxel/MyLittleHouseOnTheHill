94fa2608cf7421b746f663587cf92b68903415d2
LAPTOP-A38KUSPF\pro <catambroise@hotmail.fr>
2020-10-11 13:44:08 +0200

    replay

---
+++ Assets/Scripts/ReplayManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ReplayManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject button;
+    public void DisplayButton()
+    {
+        button.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
